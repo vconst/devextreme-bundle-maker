94a97c23e6c67037efb643c55df02223ce49badb
maximkudriavtsev <maxim_71rus_wow@mail.ru>
2020-08-26 16:26:49 +0300

    Add react app

---
+++ playground/react-app/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import NativeButton from '../../../bundles/react/button-native';
+
+function App() {
+  return (
+    <div>
+        NativeButton
+        <NativeButton />
+        
+        NativeButton
+        <NativeButton />
+        
+        NativeButton
+        <NativeButton />
+        
+        NativeButton
+        <NativeButton />
+    </div>
+  );
+}
+
+export default App;
