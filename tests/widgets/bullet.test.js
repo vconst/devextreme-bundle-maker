51a46e31493104f12e3c42f6f98b09e23cb39de6
volnyagin <vconst@users.noreply.github.com>
2021-03-02 20:02:29 +0300

    Fix ci task

--- tests/widgets/bullet.test.js
+++ tests/widgets/bullet.test.js
@@ -1,13 +1,13 @@
 const { testPerformance, logResults } = require('../helpers/performance');
 
-describe('Bullet', () => {
+describe.skip('Bullet', () => {
   afterAll(logResults);
   [
     'Minimum options',
     'Memory create',
     'Memory leaks',
   ].forEach((name) => {
-    [/*'jquery', 'react', 'vue', 'angular'*/].forEach((framework) => {
+    ['jquery', /*'react', 'vue', 'angular'*/].forEach((framework) => {
       it(`${name} ${framework}`, async () => {
         await testPerformance(name, framework, ['bullet-basic', 'bullet-renovated']);
       });
